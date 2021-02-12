9c7aa374414d684a0f9a594bbb3d5a47ef68267e
Arraiment <76941874+Arraiment@users.noreply.github.com>
2021-02-12 12:53:33 +0800

    heroku compat

--- index.js
+++ index.js
@@ -60,7 +60,7 @@ app.post('/result', (req, res) => {
 
 // add router in the Express app.
 app.use("/", router);
-app.listen(3000);
+app.listen(process.env.PORT || 3000);
 
 
 function get_links(links, choices) {
